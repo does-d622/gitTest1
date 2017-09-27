e2beadb8fbc159437daa4a34803ff38c0e5ae3cd
rivla <kazuaki@asr.info.gifu-u.ac.jp>
2017-09-27 18:17:06 +0900

    make branch feature-A

--- app/src/main/java/com/example/ideally/gittest1/MainActivity.java
+++ app/src/main/java/com/example/ideally/gittest1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ideally.gittest1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 // test comment
+// feature-A
 public class MainActivity extends AppCompatActivity {
 
     @Override
